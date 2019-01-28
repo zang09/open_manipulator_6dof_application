9b681a8060f6f7843be48119a6739ccbd96115ed
haebum <zang09@github.com>
2019-01-28 21:18:56 +0900

    update motion node

--- open_manipulator_motion/include/open_manipulator_motion/open_manipulator_motion.h
+++ open_manipulator_motion/include/open_manipulator_motion/open_manipulator_motion.h
@@ -3,10 +3,15 @@
 
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
-
+#include <eigen3/Eigen/Eigen>
 #include <sensor_msgs/JointState.h>
+
+#include "open_manipulator_msgs/OpenManipulatorState.h"
 #include "open_manipulator_msgs/SetJointPosition.h"
 #include "open_manipulator_msgs/SetKinematicsPose.h"
+#include "open_manipulator_msgs/SetDrawingTrajectory.h"
+#include "open_manipulator_msgs/SetActuatorState.h"
+#include "robotis_manipulator/robotis_manipulator.h"
 
 #include "open_manipulator_motion/MotionState.h"
 
@@ -19,11 +24,12 @@ private:
   // ROS NodeHandle
   ros::NodeHandle node_handle_;
 
-  // ROS Publisher
+  // ROS Message
   ros::Publisher open_manipulator_motion_state_pub_;
-
-  // ROS Subscribers
   ros::Subscriber open_manipulator_gui_button_sub_;
+
+  // ROS Service
+  ros::ServiceClient goal_joint_space_path_to_kinematics_pose_client_;
 
 public:
   OM_MOTION();
@@ -31,7 +37,10 @@ public:
 
   void initPublisher();
   void initSubscriber();
+  void initClient();
   void motionStatesCallback(const std_msgs::Bool::ConstPtr &msg);
+  bool setJointSpacePathToKinematicsPose(std::vector<double> kinematics_pose, double path_time);
+
 };
 }
 
--- open_manipulator_motion/src/open_manipulator_motion.cpp
+++ open_manipulator_motion/src/open_manipulator_motion.cpp
@@ -16,10 +16,15 @@
 #include "open_manipulator_motion/open_manipulator_motion.h"
 
 using namespace open_manipulator_motion;
+using namespace std;
 
 OM_MOTION::OM_MOTION()
   :node_handle_("")
-{}
+{
+  initPublisher();
+  initSubscriber();
+  initClient();
+}
 
 OM_MOTION::~OM_MOTION()
 {
@@ -40,28 +45,71 @@ void OM_MOTION::initSubscriber()
   open_manipulator_gui_button_sub_ = node_handle_.subscribe("/open_manipulator_6dof/button_clicked", 10, &OM_MOTION::motionStatesCallback, this);
 }
 
-void OM_MOTION::motionStatesCallback(const std_msgs::Bool::ConstPtr& msg)
+void OM_MOTION::initClient()
+{
+  goal_joint_space_path_to_kinematics_pose_client_ = node_handle_.serviceClient<open_manipulator_msgs::SetKinematicsPose>("goal_joint_space_path_to_kinematics_pose");
+}
+
+void OM_MOTION::motionStatesCallback(const std_msgs::Bool::ConstPtr &msg)
 {
   std::cout << "Data: " << (int)msg->data << std::endl;
+
+  std::vector<double> kinematics_pose;
+
+  kinematics_pose.push_back(0.0);
+  kinematics_pose.push_back(0.0);
+  kinematics_pose.push_back(0.467);
+  kinematics_pose.push_back(1.0);
+  kinematics_pose.push_back(0.0);
+  kinematics_pose.push_back(0.0);
+  kinematics_pose.push_back(0.0);
+
+  if(!setJointSpacePathToKinematicsPose(kinematics_pose, 2.0))
+  {
+    cout << "Fail Service!" << endl;
+    return;
+  }
+}
+
+bool OM_MOTION::setJointSpacePathToKinematicsPose(std::vector<double> kinematics_pose, double path_time)
+{
+  open_manipulator_msgs::SetKinematicsPose srv;
+
+  srv.request.end_effector_name = "gripper";
+
+  srv.request.kinematics_pose.pose.position.x = kinematics_pose.at(0);
+  srv.request.kinematics_pose.pose.position.y = kinematics_pose.at(1);
+  srv.request.kinematics_pose.pose.position.z = kinematics_pose.at(2);
+
+  srv.request.kinematics_pose.pose.orientation.w = kinematics_pose.at(3); //kinematics_pose_.pose.orientation.w;
+  srv.request.kinematics_pose.pose.orientation.x = kinematics_pose.at(4); //kinematics_pose_.pose.orientation.x;
+  srv.request.kinematics_pose.pose.orientation.y = kinematics_pose.at(5); //kinematics_pose_.pose.orientation.y;
+  srv.request.kinematics_pose.pose.orientation.z = kinematics_pose.at(6); //kinematics_pose_.pose.orientation.z;
+
+  srv.request.path_time = path_time;
+
+  cout << "call" << endl;
+
+  if(goal_joint_space_path_to_kinematics_pose_client_.call(srv))
+  {
+    return srv.response.is_planned;
+  }
+  return false;
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "open_manipulator_motion");
-  ROS_INFO("OpenManipulator 6DOF Motion node");
 
   OM_MOTION om_motion;
 
-  om_motion.initPublisher();
-  om_motion.initSubscriber();
+  ROS_INFO("OpenManipulator Motion Node");
 
   ros::Rate loop_rate(100);
-
   while (ros::ok())
   {
     ros::spinOnce();
     loop_rate.sleep();
   }
-
   return 0;
 }
